3a25a767174c8729e09c4c42e38b7d05cdfe8af1
Jonathan Morfin <jmorfin776@gmail.com>
2021-01-15 10:10:19 -0500

    fixed typo

--- src/component/profile-info/index.js
+++ src/component/profile-info/index.js
@@ -7,7 +7,7 @@ export const ProfileInfo = () => {
         <div className="profile fw-200 mt-1">
             <img src={Jonathan} alt="Jonathan Morfin"/>
             <p className="mt-1 fs-09">Hi, my name is Jonathan Morfin and I&apos;m a full stack software developer.
-                Thank you for visitng my personal website. 
+                Thank you for visiting my personal website. 
             </p>
         </div>
     )
